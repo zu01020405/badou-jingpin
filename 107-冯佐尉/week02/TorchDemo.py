94d64d401caebaefceebb1ab69ea16705ef9d92c
feng <940805240@qq.com>
2023-07-29 11:23:02 +0800

    week2 homework

---
+++ 107-冯佐尉/week02/TorchDemo.py
@@ -0,0 +1,140 @@
+# coding:utf8
+
+import torch
+import torch.nn as nn
+import numpy as np
+import random
+import json
+import matplotlib.pyplot as plt
+
+"""
+
+基于pytorch框架编写模型训练
+实现一个自行构造的找规律(机器学习)任务
+规律：
+
+"""
+
+
+class TorchModel(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(TorchModel, self).__init__()
+        self.linear = nn.Linear(input_size, num_classes)  # 线性层
+        self.activation = nn.Softmax(dim=1)  # softmax归一化函数
+        self.loss = nn.CrossEntropyLoss()  # loss函数采用交叉熵损失
+
+    # 当输入真实标签，返回loss值；无真实标签，返回预测值
+    def forward(self, x, y=None):
+        x = self.linear(x)  # (batch_size, input_size) -> (batch_size, num_classes)
+        y_pred = self.activation(x)  # (batch_size, num_classes) -> (batch_size, num_classes)
+        if y is not None:
+            return self.loss(y_pred, y)  # 预测值和真实值计算损失
+        else:
+            return y_pred  # 输出预测结果
+
+
+
+# 生成一个样本, 样本的生成方法，代表了我们要学习的规律
+#
+def build_sample():
+    x = np.random.random(5)
+    # label = np.random.choice([0, 1, 2])  # 生成 0、1、2 中的一个随机数作为标签
+    # return x, label
+    v=sum(x)/5
+    if v<.333:
+        return x,0
+    elif v<.666:
+        return x,1
+    return x,2
+
+def build_dataset(total_sample_num):
+    X = []
+    Y = []
+    for i in range(total_sample_num):
+        x, y = build_sample()
+        X.append(x)
+        Y.append(y)
+    return torch.FloatTensor(X), torch.LongTensor(Y)  # 注意将标签的数据类型改为 LongTensor
+
+
+# 测试代码
+# 用来测试每轮模型的准确率
+def evaluate(model):
+    model.eval()
+    test_sample_num = 100
+    x, y = build_dataset(test_sample_num)
+    print("本次预测集中共有%d个类别" % (len(set(y))))
+    correct = 0
+    with torch.no_grad():
+        y_pred = model(x)  # 模型预测
+        _, predicted = torch.max(y_pred, 1)  # 选择概率最大的类别作为预测结果
+        correct = (predicted == y).sum().item()  # 统计预测正确的个数
+    print("正确预测个数：%d, 正确率：%f" % (correct, correct / len(y)))
+    return correct / len(y)
+
+def main():
+    # 配置参数
+    epoch_num = 100  # 训练轮数
+    batch_size = 20  # 每次训练样本个数
+    train_sample = 5000  # 每轮训练总共训练的样本总数
+    input_size = 5  # 输入向量维度
+    num_classes = 3  # 类别数量
+    learning_rate = 0.001  # 学习率
+    # 建立模型
+    model = TorchModel(input_size, num_classes)
+    # 选择优化器
+    optim = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    log = []
+    # 创建训练集，正常任务是读取训练集
+    train_x, train_y = build_dataset(train_sample)
+    # 训练过程
+    for epoch in range(epoch_num):
+        model.train()
+        watch_loss = []
+        for batch_index in range(train_sample // batch_size):
+            x = train_x[batch_index * batch_size : (batch_index + 1) * batch_size]
+            y = train_y[batch_index * batch_size : (batch_index + 1) * batch_size]
+            loss = model(x, y)  # 计算loss
+            loss.backward()  # 计算梯度
+            if batch_index % 2 == 0:
+                optim.step()  # 更新权重
+                optim.zero_grad()  # 梯度归零
+            watch_loss.append(loss.item())
+        print("=========\n第%d轮平均loss:%f" % (epoch + 1, np.mean(watch_loss)))
+        acc = evaluate(model)  # 测试本轮模型结果
+        log.append([acc, float(np.mean(watch_loss))])
+    # 保存模型
+    torch.save(model.state_dict(), "model.pth")
+    # 画图
+    print(log)
+    plt.plot(range(len(log)), [l[0] for l in log], label="acc")  # 画acc曲线
+    plt.plot(range(len(log)), [l[1] for l in log], label="loss")  # 画loss曲线
+    plt.legend()
+    plt.show()
+    return
+
+
+# 使用训练好的模型做预测
+def predict(model_path, input_vec):
+    input_size = 5
+    num_classes = 3
+    model = TorchModel(input_size,num_classes)
+    model.load_state_dict(torch.load(model_path))  # 加载训练好的权重
+    print(model.state_dict())
+
+    model.eval()  # 测试模式
+    with torch.no_grad():  # 不计算梯度
+        result = model.forward(torch.FloatTensor(input_vec))  # 模型预测
+    for vec, res in zip(input_vec, result):
+        print(vec)
+        print(res)
+        # print("输入：%s, 预测类别：%d, 概率值：%f" % (vec, round(float(res)), res))  # 打印结果
+
+
+if __name__ == "__main__":
+    main()
+    test_vec = [[0.47889086,0.15229675,0.31082123,0.03504317,0.18920843],
+                [0.94963533,0.5524256,0.95758807,0.95520434,0.84890681],
+                [0.78797868,0.67482528,0.13625847,0.34675372,0.09871392],
+                [0.89349776,0.59416669,0.92579291,0.41567412,0.7358894]]
+    predict("model.pth", test_vec)
