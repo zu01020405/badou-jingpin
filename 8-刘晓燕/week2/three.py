bbe1bcb3b19541eeaad34f7cd9d9b8d938156e0e
monrye <chou93929@163.com>
2023-07-30 23:56:29 +0800

    [Commit]Week2_HW_by_Liuxiaoyan

--- 8-刘晓燕/week2/three.py
+++ 8-刘晓燕/week2/three.py
@@ -3,8 +3,6 @@
 import torch
 import torch.nn as nn
 import numpy as np
-import random
-import json
 import matplotlib.pyplot as plt
 
 """
@@ -20,9 +18,10 @@ class TorchModel(nn.Module):
     def __init__(self, input_size):
         super(TorchModel, self).__init__()
         self.linear = nn.Linear(input_size, 3)  # 线性层
-        ce_loss = nn.CrosseEntropyLoss()  # 交叉熵
-#        self.activation = torch.sigmoid  # sigmoid归一化函数
-#        self.loss = nn.functional.mse_loss  # loss函数采用 交叉熵 损失
+        # ce_loss = nn.functional.cross_entropy  # 交叉熵
+
+        self.activation = torch.sigmoid  # sigmoid归一化函数
+        self.loss = nn.functional.binary_cross_entropy  # loss函数采用 交叉熵 损失
 
     # 当输入真实标签，返回loss值；无真实标签，返回预测值
     def forward(self, x, y=None):
@@ -38,13 +37,12 @@ class TorchModel(nn.Module):
 # 随机生成一个3维向量，如果第一个值大于第五个值，认为是正样本，反之为负样本
 def build_sample():
     x = np.random.random(3)
-    if x[0] > x[1] and x[1] > x[2]:
-        return x, 1
+    if x[0] > x[1] > x[2]:
+        return x, [1, 0, 0]
     elif x[0] > x[1] and x[2] > x[1]:
-        return x, 0
-    elif x[2] > x[1] and x[1] > x[0]:
-        return x, -1
-
+        return x, [0, 1, 0]
+    else:
+        return x, [0, 0, 1]
 
 
 # 随机生成一批样本
@@ -56,7 +54,7 @@ def build_dataset(total_sample_num):
     for i in range(total_sample_num):
         x, y = build_sample()
         X.append(x)
-        Y.append([y])
+        Y.append(y)
 
     return torch.FloatTensor(X), torch.FloatTensor(Y)
 
@@ -66,18 +64,18 @@ def build_dataset(total_sample_num):
 def evaluate(model):
     model.eval()
     test_sample_num = 90
-    x, y ,y1= build_dataset(test_sample_num)
-    print("本次预测集中共有%d个1样本，%d个0样本，%d个-1样本" % (sum(y), test_sample_num - sum(y)-sum(y1))
-    correct, wrong = 0,0
+    x, y = build_dataset(test_sample_num)
+    y_label = torch.max(y, 1)[1]  # 获取样本标签
+    print(f"本次预测集中共有 {y.shape[0]} 个样本")
+    print("其中有样本一: %d 个; 样本二: %d 个; 样本三: %d 个" % (
+        sum(y[:, 0]), sum(y[:, 1]), sum(y[:, 2])))
+    correct, wrong = 0, 0
     with torch.no_grad():
         y_pred = model(x)  # 模型预测
-        for y_p, y_t in zip(y_pred, y):  # 与真实标签进行对比
-            if float(y_p) < 0.3 and int(y_t) == 0:
-                correct += 1  # 0样本判断正确
-            elif float(y_p) >= 0.3 and int(y_t) == 1:
-                correct += 1  # 1样本判断正确
-                float(y_p) >= 0.4 and int(y_t) == -1:
-                correct += 1  # -1样本判断正确
+        y_pred_label = torch.max(y_pred, 1)[1]
+        for y_p, y_t in zip(y_pred_label, y_label):  # 与真实标签进行对比
+            if y_p == y_t:
+                correct += 1
             else:
                 wrong += 1
     print("正确预测个数：%d, 正确率：%f" % (correct, correct / (correct + wrong)))
@@ -86,11 +84,11 @@ def evaluate(model):
 
 def main():
     # 配置参数
-    epoch_num = 10  # 训练轮数
-    batch_size = 20  # 每次训练样本个数
-    train_sample = 5000  # 每轮训练总共训练的样本总数
+    epoch_num = 100  # 训练轮数
+    batch_size = 200  # 每次训练样本个数
+    train_sample = 100000  # 每轮训练总共训练的样本总数
     input_size = 3  # 输入向量维度
-    learning_rate = 0.1  # 学习率
+    learning_rate = 0.005  # 学习率
     # 建立模型
     model = TorchModel(input_size)
     # 选择优化器
@@ -103,23 +101,23 @@ def main():
         model.train()
         watch_loss = []
         for batch_index in range(train_sample // batch_size):
-            x = train_x[batch_index * batch_size : (batch_index + 1) * batch_size]
-            y = train_y[batch_index * batch_size : (batch_index + 1) * batch_size]
+            x = train_x[batch_index * batch_size: (batch_index + 1) * batch_size]
+            y = train_y[batch_index * batch_size: (batch_index + 1) * batch_size]
             loss = model(x, y)  # 计算loss
             loss.backward()  # 计算梯度
             if batch_index % 2 == 0:
                 optim.step()  # 更新权重
                 optim.zero_grad()  # 梯度归零
             watch_loss.append(loss.item())
-        print("=========\n第%d轮平均loss:%f" % (epoch + 1, np.mean(watch_loss)))
+        print("=========\n第%d轮平均loss:%s" % (epoch + 1, np.mean(watch_loss)))
         acc = evaluate(model)  # 测试本轮模型结果
         log.append([acc, float(np.mean(watch_loss))])
     # 保存模型
     torch.save(model.state_dict(), "model.pth")
     # 画图
     print(log)
-    plt.plot(range(len(log)), [l[0] for l in log], label="acc")  # 画acc曲线
-    plt.plot(range(len(log)), [l[1] for l in log], label="loss")  # 画loss曲线
+    plt.plot(range(len(log)), [i[0] for i in log], label="acc")  # 画acc曲线
+    plt.plot(range(len(log)), [i[1] for i in log], label="loss")  # 画loss曲线
     plt.legend()
     plt.show()
     return
@@ -127,7 +125,7 @@ def main():
 
 # 使用训练好的模型做预测
 def predict(model_path, input_vec):
-    input_size = 5
+    input_size = 3
     model = TorchModel(input_size)
     model.load_state_dict(torch.load(model_path))  # 加载训练好的权重
     print(model.state_dict())
@@ -136,13 +134,14 @@ def predict(model_path, input_vec):
     with torch.no_grad():  # 不计算梯度
         result = model.forward(torch.FloatTensor(input_vec))  # 模型预测
     for vec, res in zip(input_vec, result):
-        print("输入：%s, 预测类别：%d, 概率值：%f" % (vec, round(float(res)), res))  # 打印结果
+        res_label = torch.max(res, 0)[1]
+        print("输入：%s, 预测类别：%d, 概率值：%s" % (vec, res_label, res))  # 打印结果
 
 
 if __name__ == "__main__":
-    main()
-    test_vec = [[0.47889086,0.15229675,0.31082123],
-                [0.94963533,0.5524256,0.95758807],
-                [0.78797868,0.67482528,0.13625847],
-                [0.89349776,0.59416669,0.92579291]]
+    # main()
+    test_vec = [[0.47889086, 0.15229675, 0.31082123],
+                [0.94963533, 0.5524256, 0.95758807],
+                [0.78797868, 0.67482528, 0.13625847],
+                [0.89349776, 0.59416669, 0.92579291]]
     predict("model.pth", test_vec)
